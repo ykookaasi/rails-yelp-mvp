93664420ca3ab510f8a75b14cbcef1b8fddd7f7d
Yoko Kasai <ykookaasi@gmail.com>
2020-11-11 16:06:47 +0100

    commiting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database now..."
+Restaurant.destroy_all
+puts "Database clean ✅"
+
+20.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+end
+
+puts "Done! Created #{Restaurant.count} restaurants."
